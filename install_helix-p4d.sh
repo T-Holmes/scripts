e3e11879b15e1963076ad33b92057817b00f958e
Tyler Holmes <tmh6859@rit.edu>
2020-12-17 17:34:41 -0500

    Create install_helix-p4d.sh

---
+++ install_helix-p4d.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+##################################################################
+# Name:         install_helix-p4d.sh
+# Description: 	Script to setup helix-p4d on Ubuntu bionic distro.
+# Author:	      Tyler Holmes 
+# Created On: 	December 17th 2020
+##################################################################
+
+# Add the Perforce packaging key to your APT keyring
+wget -qO - https://package.perforce.com/perforce.pubkey | sudo apt-key add -
+
+# Create source list file if it does not exist
+touch /etc/apt/sources.list.d/perforce.list
+
+# Add source for Ubuntu bionic distro
+echo deb http://package.perforce.com/apt/ubuntu boinic release >> /etc/apt/sources.list.d/perforce.list
+
+# Update sources
+apt-get update
+
+# Install helix-p4d
+apt-get install helix-p4d
